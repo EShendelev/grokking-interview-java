54bd14c6947ba7421d7430e358d22f66eb463e5c
EShendelev <sendeleve@yandex.ru>
2024-05-04 18:29:26 +0300

    oop comlete

--- grokking-interview-javatled/src/oop/service/AnimalChorus.java
+++ grokking-interview-javatled/src/oop/service/AnimalChorus.java
@@ -1,11 +1,7 @@
 package oop.service;
 
-import oop.interfaces.VocalAnimal;
 import oop.model.animals.Animal;
-import oop.model.animals.Duck;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 
 public class AnimalChorus{
